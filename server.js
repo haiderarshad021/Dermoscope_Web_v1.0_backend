45a62f41552cfa832efe6fdff45cb307e2b9ed7d
haiderarshad021 <haiderarshad021@gmail.com>
2025-12-10 14:50:38 +0500

    fixed server.js

--- server.js
+++ server.js
@@ -1,40 +1,3 @@
-// require("dotenv").config();
-// const express = require("express");
-// const cors = require("cors");
-// const authRoutes = require("./routes/authRoutes");
-// const adminRoutes = require("./routes/adminRoutes");
-// const User = require("./models/userModel");
-// const Role = require("./models/roleModel");
-
-// const app = express();
-
-// // CORS setup
-// app.use(cors({
-//   origin: "http://localhost:5173", // replace with your frontend URL
-//   credentials: true // allow cookies/session
-// }));
-
-// app.use(express.json());
-
-// // Create tables if not exists
-// Role.createTable();
-// User.createTable();
-
-// // Routes
-// app.use("/api/auth", authRoutes);
-// app.use("/api/admin", adminRoutes);
-
-// app.get("/", (req, res) => {
-//   res.json({ message: "Backend running with MySQL!" });
-// });
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () =>
-//   console.log(`Server running on port ${PORT}`)
-// );
-
-
-// server.js (or index.js)
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
@@ -67,7 +30,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Backend running with MySQL!" });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () =>
   console.log(`Server running on port ${PORT}`)
 );
